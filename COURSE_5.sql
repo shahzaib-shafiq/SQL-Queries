6e3650f3160ff0af327e8abe3146abd8898273d2
Shahzaib Shafiq <73356944+shahzaibshafiq100@users.noreply.github.com>
2023-09-10 09:53:19 +0500

    CHAP-3 COMPLETED

--- COURSE_5.sql
+++ COURSE_5.sql
@@ -271,3 +271,165 @@ SELECT
 FROM Thirds
 GROUP BY Third
 ORDER BY Third ASC;
+
+
+--CHAP 3
+WITH Athlete_Medals AS (
+  SELECT
+    Athlete,
+    COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'USA'
+    AND Medal = 'Gold'
+    AND Year >= 2000
+  GROUP BY Athlete
+)
+
+SELECT
+  Athlete,
+  Medals,
+  SUM(Medals) OVER (ORDER BY Athlete ASC) AS Max_Medals
+FROM Athlete_Medals
+ORDER BY Athlete ASC;
+
+WITH Country_Medals AS (
+  SELECT
+    Year, Country, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('CHN', 'KOR', 'JPN')
+    AND Medal = 'Gold' AND Year >= 2000
+  GROUP BY Year, Country)
+
+SELECT
+  -- Return the max medals earned so far per country
+  Country,
+  Year,
+  Medals,
+  MAX(Medals) OVER (PARTITION BY Country
+                ORDER BY Year ASC) AS Max_Medals
+FROM Country_Medals
+ORDER BY Country ASC, Year ASC;
+
+WITH France_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'FRA'
+    AND Medal = 'Gold' AND Year >= 2000
+  GROUP BY Year)
+
+SELECT
+  Year,
+  Medals,
+  min(Medals) OVER (ORDER BY Year ASC) AS Min_Medals
+FROM France_Medals
+ORDER BY Year ASC;
+
+WITH Scandinavian_Medals AS (
+  SELECT
+    Year,
+    COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('DEN', 'NOR', 'FIN', 'SWE', 'ISL')
+    AND Medal = 'Gold'
+  GROUP BY Year
+)
+
+SELECT
+  Year,
+  Medals,
+  MAX(Medals) OVER (
+    ORDER BY Year ASC
+    ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING
+  ) AS Max_Medals
+FROM Scandinavian_Medals
+ORDER BY Year ASC;
+
+
+WITH Scandinavian_Medals AS (
+  SELECT
+    Year,
+    COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('DEN', 'NOR', 'FIN', 'SWE', 'ISL')
+    AND Medal = 'Gold'
+  GROUP BY Year
+)
+
+SELECT
+  Year,
+  Medals,
+  MAX(Medals) OVER (
+    ORDER BY Year ASC
+    ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING
+  ) AS Max_Medals
+FROM Scandinavian_Medals
+ORDER BY Year ASC;
+
+
+
+WITH Chinese_Medals AS (
+  SELECT
+    Athlete, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'CHN' AND Medal = 'Gold'
+    AND Year >= 2000
+  GROUP BY Athlete)
+
+SELECT 
+  -- Select the athletes and the medals they've earned
+  Athlete,
+  Medals,
+  -- Get the max of the last two and current rows' medals 
+  MAX(Medals) OVER (ORDER BY Athlete ASC
+            ROWS BETWEEN 2 PRECEDING 
+            AND current ROW) AS Max_Medals
+FROM Chinese_Medals
+ORDER BY Athlete ASC;
+
+WITH Russian_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'RUS'
+    AND Medal = 'Gold'
+    AND Year >= 1980
+  GROUP BY Year)
+
+SELECT
+  Year, Medals,
+  --- Calculate the 3-year moving average of medals earned
+  AVG(Medals) OVER
+    (ORDER BY Year ASC
+     ROWS BETWEEN
+     2 PRECEDING AND CURRENT ROW) AS Medals_MA
+FROM Russian_Medals
+ORDER BY Year ASC;
+
+WITH Country_Medals AS (
+  SELECT
+    Year, Country, COUNT(*) AS Medals
+  FROM Summer_Medals
+  GROUP BY Year, Country)
+
+SELECT
+  Year, Country, Medals,
+  -- Calculate each country's 3-game moving total
+  SUM(Medals) OVER
+    (PARTITION BY Country
+     ORDER BY Year ASC
+     ROWS BETWEEN
+     2 PRECEDING AND CURRENT ROW) AS Medals_MA
+FROM Country_Medals
+ORDER BY Country ASC, Year ASC;
+
+--CHAP 4 
+
+
